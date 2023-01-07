5e75c817918a0d16bed5e8abb26ef762e9da7758
The-Shinigami <mohamedbarbarr@gmail.com>
2023-01-07 09:01:28 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import axios from "axios"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'say-hello-interface';
+  message:string = "";
+  @ViewChild('name') input: any;
+  
+
+  ngOnInit(){
+    this.message = "hi"
+    axios.get("http://localhost:8080/say-hello/"+this.input).then(res=>{
+      this.message = res.data;
+    })
+  }
 }
